29ccd0b73cf3e80d7ca8651dd125c27620df2cda
ADAM M SANADI <64148954+adamsanadi6@users.noreply.github.com>
2025-02-04 17:46:34 +0530

    Updated hasIncompleteEncounters and enrolmentHasDueEncounter to check encounter completeness using min and max day ranges.

--- src/global.js
+++ src/global.js
@@ -1,16 +1,17 @@
 'use strict';
 () => {
     function hasIncompleteEncounters(encounters, imports, schedule, enrolmentBaseDateConcept) {
+        if(encounters.length >= schedule.length) return false;
         const baseDate = getBaseDate(encounters[0].programEnrolment, enrolmentBaseDateConcept);
         const daysBetween = imports.moment(new Date()).diff(imports.moment(baseDate), 'days');
-        const numberOfEncountersExpected = schedule.filter(days => days <= daysBetween).length;
-        return encounters.length < numberOfEncountersExpected;
+        const day = schedule[encounters.length];
+        return day.min <= daysBetween && day.max > daysBetween;
     }
 
     function enrolmentHasDueEncounter(enrolment, imports, schedule, enrolmentBaseDateConcept) {
         const baseDate = getBaseDate(enrolment, enrolmentBaseDateConcept);
         const daysBetween = imports.moment(new Date()).diff(imports.moment(baseDate), 'days');
-        return schedule[0] <= daysBetween;
+        return schedule[0].min <= daysBetween && schedule[0].max > daysBetween;
     }
 
     function getBaseDate(enl, enrolmentBaseDateConcept) {
